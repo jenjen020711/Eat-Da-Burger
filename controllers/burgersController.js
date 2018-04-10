416dc28e6fa8105db895661494d9cf7af849442b
jensweatmon <jenjenhuynh@gmail.com>
2018-04-10 05:16:47 -0400

    made changes to mySQL database

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,61 +1,37 @@
+// Create all the functions that will do the routing for the app
 var express = require("express");
-
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
-    var hbsObject = {
-      cats: data
-    };
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function (req,res) {
+  burger.all(function (data) {
+    var hbsObject = { burgers: data };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+router.post("/burgers/create", function (req,res) {
+  burger.create([burger_name, "devoured"], [req.body.name, req.body.devoured], function() {
+    res.redirect("/burgers");
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/burgers/update/:id", function (req,res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+  console.log("condition," condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  burgers.update({ devour: req.body.devoured}, condition, function() {
+    res.redirect("/burgers");
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-// Export routes for server.js to use.
 module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+--utilize burgers_db
+USE burgers_db;
+
+
+--insert 3 burgers
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("turkey burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie burger", true);
